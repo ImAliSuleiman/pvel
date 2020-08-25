c8d9c2b3816527c79b8df8d156e841b54058fe0d
Ali Suleiman <allysuley@gmail.com>
2020-08-25 12:40:07 +0300

    Add Blip HTML code

---
+++ resources/views/blip.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Blip</title>
+</head>
+
+<body>
+
+  <header>
+    <a href="#" class="logo">Blip</a>
+    <nav>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <div class="wrapper">
+
+    <section class="main">
+      <div class="inner">
+        <div class="title-clipper">
+          <h1>Go Blip</h1>
+        </div>
+
+        <div class="sub-clipper">
+          <p>Join today and start developing software solutions like a pro!</p>
+          <a href="#" class="read-btn primary">Start Coding</a>
+        </div>
+      </div>
+    </section>
+
+
+    <section class="featured">
+      <div class="inner">
+        <div class="title-clipper">
+          <h2>Featured</h2>
+        </div>
+
+        <div class="sub-clipper">
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero magnam at inventore, laboriosam earum, voluptatum debitis magni molestiae minima, alias totam non. Maxime, velit libero sit porro asperiores molestiae aliquid, consequuntur laboriosam accusamus corrupti corporis quae optio sint eligendi vel.</p>
+          <a href="#" class="read-btn">Read More</a>
+        </div>
+      </div>
+    </section>
+
+
+    <section class="subscribe">
+      <div class="inner">
+        <div class="title-clipper">
+          <h2>Subscribe</h2>
+        </div>
+
+        <div class="sub-clipper">
+          <p>Get the lastest updates about Blip courses, offers, and opportunites.</p>
+          <input type="text" name="email" placeholder="Email Address">
+          <button class="cta">Subscribe Now</button>
+        </div>
+      </div>
+    </section>
+  </div>
+
+</body>
+
+</html>
