685202f826fdd170c9861b8b05b07c147509c427
Ali Suleiman <allysuley@gmail.com>
2020-09-08 13:14:57 +0300

    Add Biz social and contact sections design

--- resources/views/biz.blade.php
+++ resources/views/biz.blade.php
@@ -95,12 +95,71 @@
                     </ul>
                 </div>
 
-                <div class="col-md-6">
+                <div class="col-md-6 text-center">
                     <img src="images/biz-team-work.png" alt="Biz people picture" class="img-fluid" width="60%">
                 </div>
             </div>
         </div>
     </section>
+
+    <section id="testimonial">
+        <div class="container">
+            <h1 class="title text-center">Our Clients</h1>
+            <div class="row offset-1">
+                <div class="col-md-5 testimonial">
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero magnam at inventore, laboriosam earum, voluptatum debitis magni molestiae minima, alias totam non. Maxime, velit libero sit porro asperiores molestiae aliquid, consequuntur laboriosam accusamus corrupti corporis quae optio sint eligendi vel.</p>
+                    <img src="images/dev.jpg">
+                    <p class="user-details"><b>Baba Njerekera</b><br>Co-Founder at Futuhi</p>
+                </div>
+
+                <div class="col-md-5 testimonial">
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero magnam at inventore, laboriosam earum, voluptatum debitis magni molestiae minima, alias totam non. Maxime, velit libero sit porro asperiores molestiae aliquid, consequuntur laboriosam accusamus corrupti corporis quae optio sint eligendi vel.</p>
+                    <img src="images/dev.jpg">
+                    <p class="user-details"><b>Baba Njerekera</b><br>Co-Founder at Futuhi</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="social-media">
+        <div class="container text-center">
+            <p>Find Us On Social Media</p>
+            <div class="social-icons text-center">
+                <a href="#"><img src="images/biz-team-work.png" alt="Twitter icon"></a>
+                <a href="#"><img src="images/biz-team-work.png" alt="LinkedIn icon"></a>
+                <a href="#"><img src="images/biz-team-work.png" alt="GitHub icon"></a>
+                <a href="#"><img src="images/biz-team-work.png" alt="YouTube icon"></a>
+            </div>
+        </div>
+    </section>
+
+    <section id="footer">
+        <img src="images/wave-inverted.png" alt="A wave" width="100%">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 footer-box">
+                    <img src="images/biz-team-work.png" alt="Logo" width="40%">
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero magnam at inventore, laboriosam earum, voluptatum debitis magni molestiae minima, alias totam non. Maxime, velit libero sit porro asperiores molestiae aliquid, consequuntur laboriosam accusamus corrupti corporis quae optio sint eligendi vel.</p>
+                </div>
+                <div class="col-md-4 footer-box">
+                    <p class="footer-title"><b>Contact Us</b></p>
+                    <p><i class="fa fa-phone"></i> +255 111 200 200</p>
+                    <p><i class="fa fa-envelope"></i> info@biz.com</p>
+                    <p><i class="fa fa-map-pin"></i> Kwa Mwalimu</p>
+                </div>
+                <div class="col-md-4 footer-box">
+                    <p class="footer-title"><b>Subscribe</b></p>
+                    <input type="text" class="form-control" placeholder="Enter Email...">
+                    <button type="button" class="btn btn-primary">Subscribe</button>
+                </div>
+            </div>
+        </div>
+        <div>
+            <hr>
+            <p class="copyright">(c) 2020 Biz Inc.</p>
+        </div>
+    </section>
+
 </body>
 
 </html>
